d3f58b198662b475194cfaf05c00405059ee1da2
Maneesha R <101672334+maneesha2906@users.noreply.github.com>
2025-03-14 14:38:27 +0000

    Update transform_data.py

--- transform_data.py
+++ transform_data.py
@@ -24,8 +24,8 @@ df = spark.sql("SELECT * FROM {}.{}".format(HIVE_DB, SOURCE_TABLE))
 
 # ✅ Clean 'linestatus' Column
 df = (
-    df.withColumn("linestatus", regexp_replace(concat_ws(col("linestatus"), r'\\', '')))  # Remove escape characters
-    .withColumn("linestatus", regexp_replace(concat_ws(col("linestatus"), r'["\[\]]', '')))  # Remove quotes and brackets
+    df.withColumn("linestatus", regexp_replace(concat_ws(",",col("linestatus")), r'\\', ''))  # Remove escape characters
+    .withColumn("linestatus", regexp_replace(concat_ws(",",col("linestatus")), r'["\[\]]', ''))  # Remove quotes and brackets
 )
 
 logger.info("Data transformation completed successfully")
